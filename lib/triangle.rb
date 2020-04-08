4be718993b5374dba93c03b50d0882ad513ddbb4
Mariam Haider <mfh.haider@gmail.com>
2020-04-08 03:36:15 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -44,10 +44,8 @@ end
   def valid?
     (@side1 + @side2 > @side3) && (@side2 + @side3 > @side1) && (@side1 + @side3 > @side2)
   end
+  class TriangleError < StandardError
 
 end
 
-class TriangleError < StandardError
-
-end
 end
